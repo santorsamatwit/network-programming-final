99c7af35c8a186b5705d3db924d6f2880b8886eb
Christian4302B <156977117+Christian4302B@users.noreply.github.com>
2024-10-24 19:25:14 -0400

    Add files via upload

---
+++ ProjClientTCP.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 24 18:29:06 2024
+
+@author: olivi
+"""
+
+import socket
+
+HOST = '10.220.88.192'
+PORT = 8080
+FILENAME = ( )  
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+
+s.send(FILENAME.encode('utf-8'))
+
+f = open(FILENAME, 'rb')
+while True:
+   
+
